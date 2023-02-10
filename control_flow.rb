180a82b098eb44bb1431f174635f0eb60c85c658
kaltuma <kaltumanoor370@gmail.com>
2023-02-10 18:47:58 +0300

    completed assignment

--- control_flow.rb
+++ control_flow.rb
@@ -1,50 +1,63 @@
+require 'pry'
+
 def admin_login(username, password)
+  
+    if (username === "admin" || username === "ADMIN") && password === "12345"
+      "Access granted";
+    else
+      "Access denied";
+      
+    end
+    
+
   # your code here
-  if (username == "admin"  || username == "ADMIN" ) && password == "12345"
-    return "Access granted"
-  else 
-    return "Access denied"
-  end
 end
+admin_login("admin", "12345")
+
 
 def hows_the_weather(temperature)
   # your code here
-  if temperature < 40
-    "It's brisk out there!"
-  elsif temperature >=40 && temperature <=65
-    "It's a little chilly out there!"
-  elsif temperature >85
-    "It's too dang hot out there!"
-  else "It's perfect out there!"
-  end
+  response = if temperature < 40
+               "brisk";
+              elsif temperature >= 40 && temperature <= 65
+                "a little chilly";
+              elsif temperature > 85
+                "too dang hot";
+              else
+                "perfect";
+              end
+            "It's #{response} out there!";
+
 end
+hows_the_weather(20)
+
 
 def fizzbuzz(num)
-  if num%3 == 0 && num%5 ==0
+  # your code here
+  if num%3 == 0 && num%5==0
     "FizzBuzz"
-  elsif num%3 == 0
-    "Fizz"
-  elsif num%5 == 0
-    "Buzz"
-  else num
-  end
+    elsif num%3 == 0
+      "Fizz"
+      elsif num%5 == 0
+        "Buzz"
+        else
+          num
+        end
 end
+
 
 def calculator(operation, num1, num2)
   # your code here
-  case operation
-  when "+"
+  if operation == "+"
     num1 + num2
-  when "-"
+  elsif operation == "-"
     num1 - num2
-  when "*"
+  elsif operation == "*"
     num1 * num2
-  when "/"
+  elsif operation == "/"
     num1 / num2
   else
-    puts "Invalid operation!"
-    return nil
+    puts 'Invalid operation!'
+
   end
 end
-
-# calculator("+", 2, 3)
